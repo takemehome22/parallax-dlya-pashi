f1fc76c4d3774d8407377eb0aa272e890996adb7
takemehome22 <113673354+takemehome22@users.noreply.github.com>
2023-03-25 10:34:15 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,8 @@
-const bgTree = document.getElementById("two_photo");
-document.addEventListener("mousemove", function (e) {
-  MoveBackground(e);
-});
+window.onmousemove = function mouse(e) 
+{
+  document.querySelector(".space").style.backgroundPositionX = e.clientX * -0.05 + "px";
+  document.querySelector(".space").style.backgroundPositionY = e.clientY * -0.05 + "px";
 
-function MoveBackground(e) {
-    let X = (e.clientX / window.innerWidth * -30) - 15;
-    let Y = (e.clientY / window.innerHeight * -10) - 5;
-  bgTree.setAttribute(
-    "style",
-    "background-position: " + X + "px " + Y + "px;"
-  );
-}
-const space = document.getElementById("first_photo");
-document.addEventListener("mousemove", function (e) {
-  MoveBackground(e);
-});
-
-function MoveBackground(e) {
-    let X = (e.clientX / window.innerWidth * 30) - 15;
-    let Y = (e.clientY / window.innerHeight * 10) - 5;
-    space.setAttribute(
-    "style",
-    "background-position: " + X + "px " + Y + "px;"
-  );
-}
+  document.querySelector(".three").style.backgroundPositionX = e.clientX * 0.01 + "px";
+  document.querySelector(".three").style.backgroundPositionY = e.clientY * 0.01 + "px";
+};
